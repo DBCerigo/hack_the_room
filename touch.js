80c057a07c9a3c14aba5d44bec1e3c6a78bf790a
Yolijn van der Kolk <mail@yolijn.com>
2016-06-18 15:12:51 +0200

    Initial touch event listener

---
+++ touch.js
@@ -0,0 +1,12 @@
+var phone = new BFtObject();
+$(function(){
+    phone.start("172.16.27.64");
+
+    window.setTimeout(function(){phone.registerTouch()} , 1000);
+});
+
+
+function buttonPressed(evt)
+{
+	console.log(evt);
+}
