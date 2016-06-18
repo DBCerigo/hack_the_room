d4105a616a8d2a7b4bbf8e912b40495402c624bb
dbcerigo <dbcerigo@gmail.com>
2016-06-18 12:59:58 +0200

    on off functions

--- led.js
+++ led.js
@@ -1,1 +1,40 @@
 // add whatever functions in!
+
+var element=new BFtObject();
+$(function(){
+    element.start("172.16.27.34");
+}); 
+
+function setColorAll(color) {
+    var r = 0
+    var b = 0
+    var g = 0
+    if (color == 'red') {
+       r =100 
+    }
+    else if (color == 'blue') {
+       b =100 
+    }
+    else if (color == 'green') {
+       g =100 
+    }
+    else {
+        console.log('color must red, green or blue')
+        return
+    }
+    var max_num = 145
+    for (var i = 0; i < max_num; i++) {
+        element.setColorEmbedded(i,r,g,b,'null');
+    }
+}
+
+function setOff() {
+    var r = 0
+    var b = 0
+    var g = 0
+    var max_num = 145
+    for (var i = 0; i < max_num; i++) {
+        element.setColorEmbedded(i,r,g,b,'null');
+    }
+}
+
